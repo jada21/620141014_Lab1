f3de64fb809c5635aa7507e148b1a12f7b336541
Jada Campbell <mzjay92@gmail.com>
2023-01-27 15:45:04 -0500

    question2

--- app.py
+++ app.py
@@ -7,4 +7,19 @@ def parallel(*resistors):
         result = 1/sum
     return result 
 
-print (parallel(330, 1000, 2200))
+print (parallel(330, 1000, 2200), "ohms")
+print("\n")
+
+
+def potential_divider(voltage, resistor2s=[]):
+    ans = 0
+    sum2 = 0
+    for x in resistor2s:
+        sum2+= x
+
+    for x in resistor2s:
+        ans = (x/sum2)*voltage
+        print(ans, "volts")
+
+values=[3000, 1000,]
+potential_divider(9, values)
